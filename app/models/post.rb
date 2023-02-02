a118d391a61c733c5ec90bef4558bbdc903ff806
Robert Allende <allende.rob@gmail.com>
2023-02-02 12:29:48 -0600

    commit tree cea6ca6f6c7bf493789233d1ac819d3d467e4da4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :contains_the_bait 
+
+    def contains_the_bait
+        unless title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top") || title.include?("Guess")
+            errors.add(:title, "has to contain the bait")
+        end
+    end
+
 end
